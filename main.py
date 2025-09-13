ef5c8037cd3300a224e9833236c4e4a61f944f7e
David Roman <officialdavidromanyt@gmail.com>
2025-09-12 22:17:29 -0300

    added the abbility to change the gravity

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 screen_w =  1600
 screen_h =  1000
 
-
+py.init()  
 
 screen = py.display.set_mode((screen_w, screen_h))
 py.display.set_caption("ball ") 
@@ -14,15 +14,14 @@ balls = []
 
 colors = [(250,250,250), (250, 0, 250), (0, 250, 0), (0,250, 250), (0,0,250), (150, 150, 0), (230, 40, 69), (57, 67, 123) ]
 
-
+gravity = 9
+font = py.font.SysFont('Arial', 20)
 
 class Ball:
 
     def __init__(self, position):
         self.position = list(position)
-
         self.velocity = 0
-        self.gravity = 9.81
         self.mass = 2
         self.color = random.choice(colors)
 
@@ -32,7 +31,7 @@ class Ball:
 
 
     def physcis(self):
-        fg = self.mass* self.gravity
+        fg = self.mass* gravity
     
 
     
@@ -84,6 +83,13 @@ def spawn_ball(x, y):
 
 
 
+def text():
+    txt = f"gravity: {gravity} (press arrow keys to change)"
+    text_surface_1 = font.render(txt, True, (250,250,250)) 
+    text_rect_1 = text_surface_1.get_rect()
+    text_rect_1.center = (250, 25)
+    screen.blit(text_surface_1, text_rect_1)
+
 
 running = True
 while running:
@@ -94,6 +100,18 @@ while running:
         if event.type == py.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = event.pos
             spawn_ball(mouse_x, mouse_y)
+
+
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_UP:
+                gravity += 1
+            elif event.key == py.K_DOWN:
+                    gravity -= 1
+
+
+
+
+
 
 
     screen.fill((0, 0, 0)) 
@@ -108,5 +126,8 @@ while running:
         if not ball.timer():  balls.remove(ball)
 
 
+    text()
+
     py.display.flip()   
     clock.tick(100)
+
